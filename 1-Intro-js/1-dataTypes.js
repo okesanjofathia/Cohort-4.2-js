07af6b11120fe799305d938eacdc211ff15111fd
okesanjofathia <okesanjofathia@gmail.com>
2024-12-02 13:06:29 +0100

    Introduction to Javascript

---
+++ 1-Intro-js/1-dataTypes.js
@@ -0,0 +1,34 @@
+// classwork
+console.log("Okesanjo".length);
+console.log("Fathia".length);
+console.log("opeyemi".toUpperCase());
+console.log(
+  "ALL CHARACTERS TO LOWERCASE =>",
+  "I AM SUPER EXCITED TO START LEARNING JS".toLowerCase()
+);
+console.log("grandma".includes("g"));
+console.log("fathia".includes("o"));
+/**
+ * index of a string characters
+ */
+console.log("first character", "fathia"[0]);
+console.log("second character", "fathia"[1]);
+console.log("third character", "fathia"[2]);
+console.log("fourth character", "fathia"[3]);
+console.log("fifth character", "fathia"[4]);
+console.log("sixth character", "fathia"[5]);
+console.log("Classwork ==>", "fathia".slice(0, 3));
+
+console.log("fathia".split(" - "));
+/**
+ * Math Methods
+ * Math.random(), Math.floor(),Math.ceil(),Math.rand(), Math.PI()
+ */
+// Math.random(): Returns
+console.log("Math random result btw 0 and 1 ==>", Math.random());
+console.log("Math random result btw 0 and 5 ==>", Math.random() * 5);
+console.log("Math random result from 1 to 10 ==>", Math.random() * 10 + 1);
+/**
+ * (method) Math.floor(x: number): number
+ */
+console.log("Classwork ==>", Math.floor(Math.random() * 10 + 90));
